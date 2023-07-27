a1131a268323dd5c691d50ae50137cc523c0449c
willdavies0 <42768478+willdavies0@users.noreply.github.com>
2023-07-27 14:06:01 +0100

    created a post request method and moved them to static requester (#2)

--- lib/SecureRedactRequest.ts
+++ lib/SecureRedactRequest.ts
@@ -1,6 +1,54 @@
 import SecureRedactError from './SecureRedactError.ts';
 
 class SecureRedactRequest {
+  static makePostRequest = async (
+    url: string,
+    data: Record<string, string>,
+    auth: string
+  ) => {
+    try {
+      return await SecureRedactRequest.makeRequest(url, {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          Authorization: auth
+        },
+        body: JSON.stringify(data)
+      });
+    } catch (err) {
+      if (err instanceof SecureRedactError) {
+        throw err;
+      } else {
+        throw new SecureRedactError(err.message, 500);
+      }
+    }
+  };
+
+  static makeGetRequest = async (
+    url: string,
+    params: Record<string, string>,
+    auth: string
+  ) => {
+    try {
+      return await SecureRedactRequest.makeRequest(
+        `${url}?${SecureRedactRequest.buildQueryParams(params)}`,
+        {
+          method: 'GET',
+          headers: {
+            Accept: 'application/json',
+            Authorization: auth
+          }
+        }
+      );
+    } catch (err) {
+      if (err instanceof SecureRedactError) {
+        throw err;
+      } else {
+        throw new SecureRedactError(err.message, 500);
+      }
+    }
+  };
+
   static makeRequest = async (url: string, options?: RequestInit) => {
     try {
       const response = await fetch(url, options);
@@ -19,6 +67,14 @@ class SecureRedactRequest {
       } else {
         throw new SecureRedactError(err.message, 500);
       }
+    }
+  };
+
+  static buildBody = (obj: Record<string, string>) => {
+    try {
+      return JSON.stringify(obj);
+    } catch (err) {
+      throw new Error(`Failed to stringify body: ${err.message}`);
     }
   };
 
