0cc60cf317c0de17149e4c6b66e17d7a67cebc41
willdavies0 <42768478+willdavies0@users.noreply.github.com>
2023-08-04 15:32:19 +0100

    fixing typescript export (#18)
    * fixing typescript export
    
    * fixing tests
    
    * adding standard types to lib export

---
+++ lib/index.ts
@@ -0,0 +1,43 @@
+import { SecureRedactSDK } from './SecureRedactSDK';
+import {
+  SecureRedactUsername,
+  SecureRedactMediaId,
+  SecureRedactFetchMediaStatusParams,
+  SecureRedactFetchTokenParams,
+  SecureRedactBearerToken,
+  SecureRedactMediaInfo,
+  SecureRedactUserInfo,
+  SecureRedactCreateUserParams,
+  SecureRedactUploadResponse,
+  SecureRedactUploadMediaParams,
+  SecureRedactRedactMediaParams,
+  SecureRedactRedactResponse,
+  SecureRedactDeleteMediaParams,
+  SecureRedactDeleteMediaResponse,
+  SecureRedactLoginUserParams,
+  SecureRedactLoginResponse,
+  SecureRedactDownloadMediaParams,
+  SecureRedactDownloadMediaResponse
+} from './types/lib.js';
+
+export {
+  SecureRedactSDK,
+  SecureRedactMediaId,
+  SecureRedactUsername,
+  SecureRedactFetchMediaStatusParams,
+  SecureRedactFetchTokenParams,
+  SecureRedactBearerToken,
+  SecureRedactMediaInfo,
+  SecureRedactUserInfo,
+  SecureRedactCreateUserParams,
+  SecureRedactUploadResponse,
+  SecureRedactUploadMediaParams,
+  SecureRedactRedactMediaParams,
+  SecureRedactRedactResponse,
+  SecureRedactDeleteMediaParams,
+  SecureRedactDeleteMediaResponse,
+  SecureRedactLoginUserParams,
+  SecureRedactLoginResponse,
+  SecureRedactDownloadMediaParams,
+  SecureRedactDownloadMediaResponse
+};
